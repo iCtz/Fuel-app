bae6a763332cc05f43b5db04b691e25a3cdcddaf
iCtz <ahmed.t.alfusail@gmail.com>
2025-04-26 19:13:17 +0300

    fix app initialization

---
+++ android/app/src/main/java/com/example/avigna/MainActivity.java
@@ -0,0 +1,7 @@
+package com.example.avigna;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    // You can keep this empty or add custom code
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:avigna/screens/start.dart';
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // Add this if using plugins
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
